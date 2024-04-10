8ebb46b2d870e27e92ede4b78414ea8240797a2a
Estudiante-Jesed-UPT <jesed_1321124104@uptecamac.edu.mx>
2024-04-10 16:58:42 -0600

    Agregar archivos de proyecto.

---
+++ ConversionesApp/ConversionesApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using HTTPupt;
+using Newtonsoft.Json;
+namespace ConversionesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        PeticionHTTP peticion = new PeticionHTTP("https://Cervantes.bsite.net/");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+        private void Metros_Clicked(object sender, EventArgs e)
+        {
+            float numero = float.Parse(cantidad.Text);
+
+            peticion.PedirComunicacion("api/Conversiones/Metro?cantidad=" + numero, MetodoHTTP.GET,TipoContenido.JSON);
+            string respuestaJson = peticion.ObtenerJson();
+            DisplayAlert("Resultado", respuestaJson, "Aceptar");
+
+        }
+
+        private void Centimetros_Clicked(object sender, EventArgs e)
+        {
+            float numero = float.Parse(cantidad.Text);
+            peticion.PedirComunicacion("api/Conversiones/Centimetros?cantidad=" + numero, MetodoHTTP.GET, TipoContenido.JSON);
+            string respuestaJson = peticion.ObtenerJson();
+            DisplayAlert("Resultado", respuestaJson, "Aceptar");
+
+        }
+
+    }
+}
